a2cb25bc5baa221832765cec7eb13966309fab7a
TCMooney <tcmooney88@gmail.com>
2019-06-03 00:04:09 -0400

    built and styled header component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './headerNav/header';
 
 export default class Layout extends Component {
   render() {
     return (
       <div className='layout'>
-        Layout
+        {this.props.children}
+        <Header/>
       </div>
     );
   }
